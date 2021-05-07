17964bdc52d2033d618474239831aeefa0e13211
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-07 08:15:20 -0300

    Começando resolver.

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Notas da disciplina
+
+Faça um programa contendo uma função que como argumentos os nomes de dois 
+arquivos. O primeiro arquivo armazena os nomes dos alunos e o segundo arquivo 
+contém as respectivas notas dos alunos. O nome em uma linha do primeiro arquivo 
+está associado às notas (uma ou mais) na linha correspondente do segundo arquivo.
+Assuma que as notas estão separadas umas das outras por virgulas. A função deve 
+ler os dois arquivos e imprimir como saída o nome do aluno e a respectiva média 
+de suas notas. No programa princimpal deve ser chamado tal função.
+
+Exemplo de arquivos de entrada:
+
+-------------------------------
+Joao                   |8,5,9            
+Maria                  |9,5,5
+Jose                   |8,9,3
+-------------------------------
+
+
+Exemplo de saída:
+
+-------------------------------
+Nome         Média
+
+Joao         7.33
+Maria        6.33
+Jose         6.67
+-------------------------------
+
+"""
+
+
+
+
+
+
