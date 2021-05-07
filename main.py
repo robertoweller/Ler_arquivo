9d23d7528f03d3707f000c499bbb6a7ec08a12ca
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-07 11:35:43 -0300

    resolvido

--- main.py
+++ main.py
@@ -30,8 +30,32 @@ Jose         6.67
 
 """
 
+def leiaArquivos(nomes, notas):
+    """ 
+    Ler os dois arquivos e imprimir como saída o nome do aluno e a respectiva
+    média de suas notas
+    """
+    nomes = open(nomes, 'r')
+    notas = open(notas, 'r')
+    listaNome = ''.join(nomes).split()
+    listaNota = ''.join(notas).split()
+
+    print(f'\n{"Nome":20}Média')
+
+    for nm in enumerate(listaNome):
+        # Convertendo cada item da lista em inteiros, pq são strings
+        media = list(map(int, listaNota[nm[0]].split(',')))
+        media = sum(media)/len(media)
+
+        print(f'{nm[1]:20}{media:.2f}')
+    
+    nomes.close()
+    notas.close()
 
 
+if __name__ == "__main__":
+    nomes_alunos = 'nomes_dos_alunos.txt'
+    notas_alunos = 'notas_dos_alunos.txt'
 
-
+    leiaArquivos(nomes_alunos, notas_alunos)
 
